929039fba261f2a16f58d9d7eaec4a1d01648cf3
Morl99 <choffmann87@gmail.com>
2021-12-04 01:00:33 +0100

    Include initial version of oruga without bulma.

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { createApp } from "vue"
 import App from "@/App.vue"
 import store from "@/store"
 import createRouter from "@/router"
+import Oruga from '@oruga-ui/oruga-next'
+import '@oruga-ui/oruga-next/dist/oruga-full-vars.css';
 
-createApp(App).use(store).use(createRouter(store)).mount("#app")
+if (import.meta.env.MODE === "development") {
+  const { worker } = await import("./mocks/browser")
+  worker.start({
+    onUnhandledRequest(req) {
+      console.error("Found an unhandled %s request to %s", req.method, req.url.href)
+    },
+  })
+}
+
+createApp(App).use(store).use(createRouter(store)).use(Oruga, {iconPack: 'fas'}).mount("#app")
