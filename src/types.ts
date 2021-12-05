7dca7d9b8816d85a4f48fcc27968beb7311fe960
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-12-05 14:15:06 +0100

    Lists recipes

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,59 @@ export type AppError = {
   message: string
   details?: unknown
 }
+
+export type User = {
+  id: string
+  firstName: string
+}
+
+export type Item = {
+  id: string
+  amount: number
+}
+
+export type Dish = {
+  id: string
+  name: string
+  recipe?: string
+  source?: string
+  last?: string
+  items: Item[]
+}
+
+export type Proposal = {
+  date: string
+  dishId: string
+  served: boolean
+}
+
+export type Ingredient = {
+  id: string
+  name: string
+  unit: string
+  group: string
+}
+
+export type CompleteItem = {
+  id: string
+  name: string
+  unit: string
+  group: {
+    id: string
+    title: string
+    order: number
+  }
+  amount: number
+  originalAmount?: number
+}
+
+export type Unit = {
+  id: string
+  name: string
+  increment: number
+}
+
+export type ItemGroup = {
+  order: number
+  title: string
+}
