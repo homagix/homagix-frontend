1d44c7f9f3a41d4af98fb6a064e51120c9f569cd
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-12-07 20:22:31 +0100

    Removes yet unused types

--- src/types.ts
+++ src/types.ts
@@ -3,11 +3,6 @@ export type AppError = {
   details?: unknown
 }
 
-export type User = {
-  id: string
-  firstName: string
-}
-
 export type Item = {
   id: string
   amount: number
@@ -20,41 +15,4 @@ export type Dish = {
   source?: string
   last?: string
   items: Item[]
-}
-
-export type Proposal = {
-  date: string
-  dishId: string
-  served: boolean
-}
-
-export type Ingredient = {
-  id: string
-  name: string
-  unit: string
-  group: string
-}
-
-export type CompleteItem = {
-  id: string
-  name: string
-  unit: string
-  group: {
-    id: string
-    title: string
-    order: number
-  }
-  amount: number
-  originalAmount?: number
-}
-
-export type Unit = {
-  id: string
-  name: string
-  increment: number
-}
-
-export type ItemGroup = {
-  order: number
-  title: string
 }
