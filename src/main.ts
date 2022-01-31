74db3be1a260fe626ff57a12d733802ae030e5ab
Morl99 <choffmann87@gmail.com>
2022-01-31 23:44:41 +0100

    Update to latest oruga and include some initial styling add initial navbar and menu

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import App from "@/App.vue"
 import store from "@/store"
 import createRouter from "@/router"
 import Oruga from '@oruga-ui/oruga-next'
-import '@oruga-ui/oruga-next/dist/oruga-full-vars.css';
+import { bulmaConfig } from "@oruga-ui/theme-bulma"
+// Could be replaced in the future with a sass import
+import "@oruga-ui/theme-bulma/dist/bulma.css"
+import "@fortawesome/fontawesome-free/scss/fontawesome.scss"
+import "@fortawesome/fontawesome-free/scss/regular.scss"
+import "@fortawesome/fontawesome-free/scss/solid.scss"
 
 if (import.meta.env.MODE === "development") {
   const { worker } = await import("./mocks/browser")
@@ -14,4 +19,21 @@ if (import.meta.env.MODE === "development") {
   })
 }
 
-createApp(App).use(store).use(createRouter(store)).use(Oruga, {iconPack: 'fas'}).mount("#app")
+createApp(App)
+  .use(store)
+  .use(createRouter(store))
+  .use(Oruga, {
+    customIconPacks: {
+      fas: {
+        sizes: {
+          default: "",
+          small: "fa-sm",
+          medium: "fa-lg",
+          large: "fa-2x",
+        },
+      },
+    },
+    iconPack: "fas",
+    ...bulmaConfig,
+  })
+  .mount("#app")
