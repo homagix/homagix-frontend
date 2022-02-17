4ecca82a8f356306901b0a13cfb22db1cc6905dd
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-02-17 06:48:44 +0100

    Reads data from homagix server by default

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ type FetchData = Record<string, string>
 type FetchOptions = Record<string, string | string[] | Record<string, string>>
 
 export async function fetchFromBackend(method: string, path: string, data: FetchData = {}, options: FetchOptions = {}) {
+  const basePath = localStorage.getItem("basePath") || "https://homagix.dilab.co"
   if (["post", "put", "patch"].includes(method.toLowerCase()) && Object.keys(data).length) {
     options.data = JSON.stringify(data)
     options.headers = { ...((options.headers || {}) as Record<string, string>), ["Content Type"]: "application/json" }
@@ -9,7 +10,7 @@ export async function fetchFromBackend(method: string, path: string, data: Fetch
     path += `?${new URLSearchParams(data).toString()}`
   }
   options.method = method
-  const response = await fetch(path, options)
+  const response = await fetch(basePath + path, options)
   const content = response.headers.get("content-type")?.match(/json/) ? await response.json() : await response.text()
   if (!response.ok) {
     const error = {
