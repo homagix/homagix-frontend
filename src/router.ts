ae7734e9bd1632b9d4f16ae9fe016a4656bc8ba4
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-02-22 04:22:32 +0100

    Works with a base url (e.g. in Github pages)

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router"
 import RecipesList from "@/components/RecipesList.vue"
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { name: "home", path: "/", redirect: "/recipes" },
     { name: "recipes", path: "/recipes", component: RecipesList },
