ae6b73a008a871cfa8e16e5a6ef185dd92e95f9f
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-03-02 06:41:17 +0100

    Uses index page with any route for vue-router to handle routes

---
+++ src/components/NotFoundPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h2>Hast du dich verirrt?</h2>
+  <p>
+    Die angeforderte Seite existiert leider nicht.
+  </p>
+  <router-link to="/">Bring mich in Sicherheit!</router-link>
+</template>
+
+<style scoped lang="scss">
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import RecipesList from "@/components/RecipesList.vue"
 import Recipe from "@/components/Recipe.vue"
+import NotFoundPage from "@/components/NotFoundPage.vue"
 
 export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,5 +9,6 @@ export default createRouter({
     { name: "home", path: "/", redirect: "/recipes" },
     { name: "recipes", path: "/recipes", component: RecipesList },
     { name: "recipe", path: "/recipes/:id", component: Recipe },
+    { path: "/:pathMatch(.*)*", component: NotFoundPage },
   ],
 })
