21ed48df8b82f94ab5cbcb302a0e51001e3d3c85
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-03-08 06:31:36 +0100

    [fix] fetches with POST method. [fix] test:unit npm script [fix] router warning in RecipesList test

--- src/components/RecipesList.test.ts
+++ src/components/RecipesList.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest"
+import { describe, expect, it, vi } from "vitest"
 import { mount } from "@vue/test-utils"
 import RecipesList from "@/components/RecipesList.vue"
 import { createStore } from "vuex"
@@ -8,6 +8,10 @@ const testDishes = [
   { id: "1", name: "dish 1", items: [], alwaysOnList: false, image: "dish1.jpg" },
   { id: "2", name: "dish 2", items: [], alwaysOnList: false, image: "dish2.jpg" },
 ] as Dish[]
+
+vi.mock("vue-router", () => ({
+  useRouter: () => ({ push: vi.fn() }),
+}))
 
 describe("RecipesList", () => {
   it("should render", () => {
