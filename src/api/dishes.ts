6700fd0640a5fec5f2f36a2b60a032a7b6fb801a
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-04-18 20:30:28 +0200

    Editable title and recipe of dishes

--- src/api/dishes.ts
+++ src/api/dishes.ts
@@ -1,3 +1,4 @@
+import { Dish } from "@/types"
 import { fetchFromBackend } from "."
 
 export async function loadDishes() {
@@ -6,4 +7,8 @@ export async function loadDishes() {
 
 export async function setFavorite(dishId: string, isFavorite: boolean) {
   return await fetchFromBackend("PATCH", `/dishes/${dishId}`, { isFavorite })
+}
+
+export async function updateDish(dish: Dish) {
+  return await fetchFromBackend("PATCH", `/dishes/${dish.id}`, dish)
 }
---
+++ src/components/IngredientsList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { Ingredient } from "@/types"
+
+defineProps<{ ingredients: Ingredient[] }>()
+</script>
+
+<template>
+  <div class="ingredient-list">
+    <template v-for="ingredient in ingredients" :key="ingredient.id">
+      <span>{{ ingredient.amount }}</span>
+      <span>{{ ingredient.unit }}</span>
+      <span>{{ ingredient.name }}</span>
+    </template>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.ingredient-list {
+  display: inline-grid;
+  grid-template-rows: auto;
+  grid-template-columns: auto auto 1fr;
+  border: 1px solid #888;
+  padding: 3px;
+  align-content: flex-start;
+
+  > * {
+    padding: 3px;
+  }
+
+  > *:nth-child(3n-2) {
+    text-align: right;
+  }
+}
+</style>
