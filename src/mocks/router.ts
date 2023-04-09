491d011bbb37807d13c08b4d451e76051d488869
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2023-04-09 12:06:46 +0200

    chore: adapts to new vue version

--- src/mocks/router.ts
+++ src/mocks/router.ts
@@ -1,7 +1,8 @@
+import { defineComponent } from "vue"
 import { createWebHistory } from "vue-router"
 import { createRouter } from "vue-router"
 
-export function createRouterMock<C>(component: C, extraPaths: string[] = []) {
+export function createRouterMock(component: ReturnType<typeof defineComponent>, extraPaths: string[] = []) {
   const history = createWebHistory()
   const routes = ["/", ...extraPaths].map(path => ({ path, component }))
   return createRouter({ history, routes })
